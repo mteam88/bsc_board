5e9174a31bab4be6c690e59bd886ca5d7743aca8
Matthew <matthew8edelen@gmail.com>
2023-03-30 20:23:23 -0400

    modify dispatch rules

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,23 @@ mod dweet;
 
 use ethers::{
     providers::{Middleware, Provider},
-    types::{Transaction, H256}, abi::AbiEncode,
+    types::{Transaction, H256},
 };
 use ethers_providers::{Http, ProviderExt, StreamExt};
 use eyre::Result;
 
+const UPGRADE_SELECTOR: [u8; 4] = 0x99a88ec4_u32.to_be_bytes();
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let provider = Provider::<Http>::connect("https://bsc-dataseed1.binance.org/").await;
+    let provider = Provider::<Http>::connect("https://bsc-dataseed2.binance.org/").await;
     let mut stream = provider.watch_blocks().await?;
     // call digest on every block in stream
     while let Some(block) = stream.next().await {
-        let block = provider.get_block_with_txs(block).await?.unwrap_or_default();
+        let block = provider
+            .get_block_with_txs(block)
+            .await?
+            .unwrap_or_default();
         block.transactions.iter().for_each(|tx| {
             tokio::spawn(digest(tx.clone()));
         });
@@ -27,23 +31,18 @@ async fn digest(tx: Transaction) -> Result<()> {
     // do something with tx
     let hash = tx.hash;
     // check if tx is a contract upgrade
-    if is_upgrade(tx).await {
-        tokio::spawn(dispatch_upgrade(hash));
+    if !(tx.input.len() < 4) || tx.input[0..4] == UPGRADE_SELECTOR || tx.to == None {
+        if tx.input[0..4] == UPGRADE_SELECTOR {
+            // if tx is a contract upgrade, dispatch immediately
+            dispatch_upgrade(format!("upgrade:{}", hash.to_string())).await?;
+        } else if tx.to == None {
+            dispatch_upgrade(format!("deploy:{}", hash.to_string())).await?;
+        }
     }
     Ok(())
 }
 
-async fn is_upgrade(tx: Transaction) -> bool {
-    // check if tx is a contract upgrade by checking first 4 bytes of data
-    if tx.input.len() < 4 {
-        return false;
-    }
-    // tx.input[0] == 169
-    tx.input[0..4] == 0x99a88ec4_u32.to_be_bytes()
-}
-
-async fn dispatch_upgrade(hash: H256) -> Result<()> {
-    let msg = format!("{}:upgrade", hash.encode_hex());
+async fn dispatch_upgrade(msg: String) -> Result<()> {
     // push to a dweet webhook
     let mut thing = dweet::Thing::new("bsc_board");
     thing.update(msg.as_str()).await.unwrap();
